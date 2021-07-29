ca611cc9a8654889592b50ddbee339b2ca6d7a90
Taylor Hogan <taylorb.hogan@gmail.com>
2021-07-28 21:31:08 -0700

    Plug in welcome message

--- react-app/src/components/HabitDashWelcome/index.js
+++ react-app/src/components/HabitDashWelcome/index.js
@@ -1,9 +1,15 @@
+import { useSelector } from 'react-redux'
+
 import styles from './HabitDashWelcome.module.css'
 
 const HabitDashWelcome = () => {
+
+  const user = useSelector(state => state.session.user)
+
   return (
     <div className={styles.container}>
-      <div>Welcome from HabitDashWelcome</div>
+      <div>Hello, {user.first_name}.</div>
+      <div>What will you achieve today?</div>
       <div></div>
     </div>
   )
