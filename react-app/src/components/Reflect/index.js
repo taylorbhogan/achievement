736f372aa6e100d63e574fe590f24309af0144fd
Taylor Hogan <taylorb.hogan@gmail.com>
2021-07-29 12:03:25 -0700

    Connect reflect and achievements routes

---
+++ react-app/src/components/Reflect/index.js
@@ -0,0 +1,11 @@
+import styles from './Reflect.module.css'
+
+const Reflect = () => {
+  return (
+    <div>
+      <div className={styles.hello}>Hello from Reflect</div>
+    </div>
+  )
+}
+
+export default Reflect
