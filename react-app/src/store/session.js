7852e8ae4346594cf8dc4ba389a285a84fff93a0
Taylor Hogan <taylorb.hogan@gmail.com>
2021-08-06 12:48:22 -0700

    Unload habits

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -99,8 +99,10 @@ export const signUp = (firstName, lastName, email, password) => async (dispatch)
 }
 
 export default function reducer(state = initialState, action) {
+  // console.log('action---------->',action);
   switch (action.type) {
     case SET_USER:
+      console.log('action---------->',action);
       return { user: action.payload }
     case REMOVE_USER:
       return { user: null }
