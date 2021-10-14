1b9cb207fbcf78fce4f729a6d8925f0f36811fca
Taylor Hogan <taylorb.hogan@gmail.com>
2021-10-14 10:46:15 -0700

    Remove comments from HabitDash

--- react-app/src/components/HabitDash/index.js
+++ react-app/src/components/HabitDash/index.js
@@ -8,9 +8,6 @@ import HabitDashWelcome from "../HabitDashWelcome"
 import HabitDashMenu from "../HabitDashMenu";
 import HabitDashCard from "../HabitDashCard"
 import LoadingContent from "../parts/LoadingContent";
-// import CreateHabitButton from "../CreateHabitButton"
-// import LoadingContent from "../LoadingContent"
-import NoHabits from "../parts/NoHabits";
 import styles from './HabitDash.module.css'
 
 function HabitDash() {
@@ -20,43 +17,15 @@ function HabitDash() {
   const user = useSelector(state => state.session.user)
   const userId = user.id
 
-  // const [ habits, setHabits ] = useState([])
-  // const [ isLoaded, setIsLoaded ] = useState(false)
   const [ showMenu, setShowMenu ] = useState(false)
-
-
-  // const unloadTest = () => dispatch(unloadHabits())
-  // window.unloadTest = unloadTest
-
 
   useEffect(() => {
     dispatch(getHabits(userId))
     return () => {
-      // console.log('I am in the RETURN useEffect HabitDash component--------------------');
 
       dispatch(unloadHabits())
     }
   }, [dispatch, userId])
-
-  // useEffect(() => {
-  //   if (Object.keys(reduxHabits).length > 0) {
-  //     setIsLoaded(true)
-  //     setHabits(Object.values(reduxHabits))
-  //   }
-  // }, [reduxHabits])
-  // console.log('I am in the HabitDash component--------------------');
-
-  // useEffect(() => {
-  //   if (!user.id) {
-  //     return;
-  //   }
-  //   (async () => {
-  //     const response = await fetch(`/api/achievements/users/${user.id}/targets`);
-  //     const targetNums = await response.json();
-  //     setTargetNumerators(targetNums);
-
-  //   })();
-  // }, [user.id]);
 
   const getToday = (daysAgo) => {
     const date = new Date()
@@ -68,26 +37,6 @@ function HabitDash() {
   const openMenu = () => {
     setShowMenu(!showMenu)
   }
-
-  // useEffect(() => {
-  //   if (!showMenu) return;
-  //   const closeMenu = () => {
-
-  //     setShowMenu(false)
-  //   }
-  //   document.addEventListener('click',closeMenu)
-
-
-  //   return () => document.removeEventListener('click',closeMenu)
-  // }, [showMenu])
-
-  // if (!isLoaded){
-  //   return <LoadingContent />
-  // }
-  // console.log('--------------------->',getToday(4));
-  // console.log('--------------------->',getToday(4).length);
-  // console.log('--------------------->',getToday(3));
-  // console.log('--------------------->',getToday(3).length);
 
   return (
     <div className={styles.container}>
@@ -103,9 +52,7 @@ function HabitDash() {
         {showMenu && <HabitDashMenu />}
       </div>
       <div className={styles.divider}></div>
-      {/* <div className={styles.makeMeRight}>
-        <CreateHabitButton />
-      </div> */}
+
       <div className={styles.dashCardContainer}>
         <div className={styles.cubeContainerHeadersWrapper}>
           <div className={styles.name}></div>
@@ -120,16 +67,12 @@ function HabitDash() {
           </div>
           <div className={styles.target}></div>
         </div>
-        {/* {habits.filter(habit => habit.name !== 'DELETED').map((habit, idx) => ( */}
-        {reduxHabits.length === 0 &&
-          <LoadingContent />
-        }
+        {reduxHabits.length === 0 && <LoadingContent />}
         {reduxHabits.filter(habit => habit.name !== 'DELETED').map((habit, idx) => (
           <HabitDashCard
             key={idx}
             habit={habit}
-            // isLoaded={isLoaded}
-          /> ))}
+          />))}
       </div>
     </div>
   )
