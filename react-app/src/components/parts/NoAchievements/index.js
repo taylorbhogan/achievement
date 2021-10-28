b42f34c7d6b6e2e8eb645223e370a9729392c044
Taylor Hogan <taylorb.hogan@gmail.com>
2021-10-28 15:48:12 -0700

    Complete loading implementation for achievements

--- react-app/src/components/parts/NoAchievements/index.js
+++ react-app/src/components/parts/NoAchievements/index.js
@@ -1,13 +1,12 @@
-import CreateHabitButton from '../../CreateHabitButton'
+import { Link } from 'react-router-dom'
 import styles from './NoAchievements.module.css'
 
 const NoAchievements = () => {
   return (
     <div className={styles.container}>
       <div className={styles.text}>Add your first achievement</div>
-      <CreateHabitButton componentStyle={'green'}/>
+      <Link className={styles.link} to='/' >Let's Go</Link>
     </div>
-
   )
 }
 
