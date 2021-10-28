7aee65918be325b19b972bcfff51136626834822
Taylor Hogan <taylorb.hogan@gmail.com>
2021-10-28 14:57:31 -0700

    Style cards

--- react-app/src/components/AchievementLogComponents/AchievementLog/index.js
+++ react-app/src/components/AchievementLogComponents/AchievementLog/index.js
@@ -3,66 +3,40 @@ import { useDispatch, useSelector } from "react-redux"
 import { getAchievements } from "../../../store/achievement"
 import { getHabits } from "../../../store/habit"
 import LoadingContent from "../../parts/LoadingContent"
-import NoHabits from "../../parts/NoHabits"
 import AchievementLogCard from "../AchievementLogCard"
 import styles from './AchievementLog.module.css'
 
 const AchievementLog = () => {
-  // const [ achievements, setAchievements ] = useState([])
   const [ isLoaded, setIsLoaded ] = useState(false)
 
   const dispatch = useDispatch()
-  // const reduxAchievements = useSelector(state => Object.values(state.achievements.achievements))
-  // console.log('reduxAchievements', reduxAchievements);
-  // const reduxAchievementsSorted = useSelector(state => Object.values(state.achievements.achievements).sort((first, second) => Date(second.created_at) - Date(first.created_at)))
-  // console.log('reduxAchievementsSorted',reduxAchievementsSorted);
-  const reduxHabits = useSelector(state => Object.values(state.habits.habits))
+
+  const habits = useSelector(state => Object.values(state.habits.habits))
   const user = useSelector(state => state.session.user)
 
   useEffect(() => {
     dispatch(getHabits(user.id))
   },[user.id, dispatch])
 
-      // IRINA SORT
-  const reduxAchievements = useSelector(state => Object.values(state.achievements.achievements).sort((first, second) => {
+  const achievements = useSelector(state => Object.values(state.achievements.achievements).sort((first, second) => {
     if (new Date(first.created_at) > new Date(second.created_at)) return -1
     if (new Date(first.created_at) < new Date(second.created_at)) return 1
     return 0
       }))
-  // NO SORTING
-  // const reduxAchievements = useSelector(state => Object.values(state.achievements.achievements))
-      // console.log('reduxAchievements---------->', reduxAchievements);
-  // const user = useSelector(state => state.session.user)
+
 
   useEffect(() => {
     dispatch(getAchievements(user.id))
   }, [dispatch, user.id])
 
-
-
-
-
-
-  // useEffect(() => {
-  //   if (Object.keys(achievements).length > 0) {
-  //     console.log('reduxAchievements',reduxAchievements);
-  //     setIsLoaded(true)
-  //     setAchievements(Object.values(reduxAchievements))
-  //   }
-  // }, [])
-
-  // if (!isLoaded){
-  //   return <LoadingContent />
-  // }
-
   return (
     <div className={styles.container}>
       <div className={styles.header}>Everything you've achieved.</div>
       <div className={styles.achievementContainer}>
-      {(reduxHabits.length === 0 || (reduxHabits.length > 0 && reduxAchievements.length === 0)) &&
+      {(habits.length === 0 || (habits.length > 0 && achievements.length === 0)) &&
           <LoadingContent />
         }
-        {reduxAchievements.length > 0 && reduxAchievements.filter(achievement => achievement.habit !== 'DELETED').map((achievement, idx) => {
+        {achievements.length > 0 && achievements.filter(achievement => achievement.habit !== 'DELETED').map((achievement, idx) => {
             return <AchievementLogCard
               key={idx}
               achievement={achievement}
