95ecdde91c3c0d72ac8511e4751c40e2e1c22e66
Taylor Hogan <taylorb.hogan@gmail.com>
2021-10-29 16:31:36 -0700

    Refactor form

--- react-app/src/components/HabitLogCardDetailsEdit/index.js
+++ react-app/src/components/HabitLogCardDetailsEdit/index.js
@@ -1,8 +1,6 @@
 import InputField from '../parts/InputField'
-import ActionButton from '../parts/ActionButton'
 
 import styles from './HabitLogCardDetailsEdit.module.css'
-
 
 const HabitLogCardDetailsEdit = ({
   blurb,
@@ -14,40 +12,36 @@ const HabitLogCardDetailsEdit = ({
 }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.top}>
-        <div className={styles.info}>
-          <div className={styles.left}>
-            <div className={styles.header}>Achievement:</div>
-            <InputField
-              name='blurb'
-              type='textarea'
-              placeholder='15x 3/day'
-              value={blurb}
-              onChange={(e) => setBlurb(e.target.value)}
-            />
-          </div>
-          <div className={styles.center}>
-            <div className={styles.header}>Stellar Achievement:</div>
-            <InputField
-              name='stellar_blurb'
-              type='textarea'
-              placeholder='100 in one day'
-              value={stellarBlurb}
-              onChange={(e) => setStellarBlurb(e.target.value)}
-            />
-          </div>
-          <div className={styles.right}>
-            <div className={styles.header}>Weekly Target:</div>
-            <div className={styles.target}>
-              <InputField
-                name='target'
-                type='number'
-                placeholder='7'
-                value={target}
-                onChange={(e) => setTarget(e.target.value)}
-              />
-            </div>
-          </div>
+      <div className={styles.left}>
+        <div className={styles.header}>Achievement:</div>
+        <InputField
+          name='blurb'
+          type='textarea'
+          placeholder='15x 3/day'
+          value={blurb}
+          onChange={(e) => setBlurb(e.target.value)}
+        />
+      </div>
+      <div className={styles.center}>
+        <div className={styles.header}>Stellar Achievement:</div>
+        <InputField
+          name='stellar_blurb'
+          type='textarea'
+          placeholder='100 in one day'
+          value={stellarBlurb}
+          onChange={(e) => setStellarBlurb(e.target.value)}
+        />
+      </div>
+      <div className={styles.right}>
+        <div className={styles.header}>Weekly Target:</div>
+        <div className={styles.target}>
+          <InputField
+            name='target'
+            type='number'
+            placeholder='7'
+            value={target}
+            onChange={(e) => setTarget(e.target.value)}
+          />
         </div>
       </div>
     </div>
