00dfce29cfca2096f3b1b414949b64d91eb88965
Taylor Hogan <taylorb.hogan@gmail.com>
2021-10-28 19:05:32 -0700

    Style delete confirmation

--- react-app/src/components/parts/DeleteConfirmationButton/index.js
+++ react-app/src/components/parts/DeleteConfirmationButton/index.js
@@ -3,74 +3,21 @@ import { FaTrashAlt } from 'react-icons/fa'
 import { IconContext } from "react-icons";
 import CloseButton from "../CloseButton";
 import DeleteButton from "../DeleteButton";
-import DeleteConfirmation from "../DeleteConfirmation";
 import styles from './DeleteConfirmationButton.module.css'
 
 const DeleteConfirmationButton = ({ handleDelete }) => {
   const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false)
 
   const deleteHandler = () => {
-    handleDelete()
     setShowDeleteConfirmation(false)
-  }
-
-  const handleClick = () => {
-    console.log('click');
-    setShowDeleteConfirmation(true)
-  }
-
-  const otherStyles = {
-    div: {
-      position: 'absolute',
-      top: '-60px',
-      left: '90px',
-      minWidth: '135px',
-      fontSize: '13px',
-      padding: '10px',
-      backgroundColor: 'var(--gray3)',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      borderRadius: 'var(--borderRadiusMenu)',
-    },
-    top: {
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'flex-start',
-      justifyContent: 'space-between',
-      width: '100%',
-    }
-  }
-
-
-  const delStyles = {
-    div: {
-      position: 'absolute',
-      top: '-50px',
-      left: '102%',
-      minWidth: '135px',
-      fontSize: '13px',
-      padding: '10px',
-      backgroundColor: 'var(--gray3)',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      borderRadius: 'var(--borderRadiusMenu)',
-    },
-    top: {
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'flex-start',
-      justifyContent: 'space-between',
-      width: '100%',
-    }
+    handleDelete()
   }
 
   return (
     <>
       <button
         className={styles.button}
-        onClick={handleClick}
+        onClick={() => setShowDeleteConfirmation(true)}
         id='deleteConfirmationButton'
       >
         <IconContext.Provider value={{ className: "react-icons" }}>
@@ -78,19 +25,15 @@ const DeleteConfirmationButton = ({ handleDelete }) => {
         </IconContext.Provider>
       </button>
       {showDeleteConfirmation &&
-        // <DeleteConfirmation
-        //   handleDelete={deleteHandler}
-        //   closeDeleteConfirmation={() => setShowDeleteConfirmation(false)}
-        //   componentLocation={'habitLog'} />
-        <div style={delStyles.div}>
-          <div style={delStyles.top}>
+        <div className={styles.div}>
+          <div className={styles.top}>
             <div>
-              <div>Deleting is</div>
-              <div>permanent.</div>
+              <div>Deleting is permanent.</div>
+              {/* <div>permanent.</div> */}
             </div>
-            <CloseButton closeForm={() => setShowDeleteConfirmation(false)} />
+            {/* <CloseButton closeForm={() => setShowDeleteConfirmation(false)} /> */}
           </div>
-          <DeleteButton handleDelete={handleDelete} />
+          <DeleteButton handleDelete={deleteHandler} />
         </div>
       }
     </>
